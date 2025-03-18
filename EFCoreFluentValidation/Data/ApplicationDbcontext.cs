e890327055879b47bf4a07333a1c70fcb86f6462
gurau-anil <gurau.anil@gmail.com>
2025-03-18 16:21:17 -0700

    Approach 2 to connect to the database (Registering Application dbContext to the service container)

--- EFCoreFluentValidation/Data/ApplicationDbcontext.cs
+++ EFCoreFluentValidation/Data/ApplicationDbcontext.cs
@@ -4,17 +4,24 @@ namespace EFCoreFluentValidation.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        private readonly IConfiguration _configuration;
+        //Approach 1 to connect to the database
+        //private readonly IConfiguration _configuration;
 
-        public ApplicationDbContext(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
+        //public ApplicationDbContext(IConfiguration configuration)
+        //{
+        //    _configuration = configuration;
+        //}
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    var connectionString = _configuration.GetConnectionString("DefaultConnection");
+        //    optionsBuilder.UseSqlServer(connectionString);
+        //}
+
+        //Appproach 2 to connect to the database.
+        //We need to register ApplicationDbContext in the service container.
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-            var connectionString = _configuration.GetConnectionString("DefaultConnection");
-            optionsBuilder.UseSqlServer(connectionString);
         }
     }
 }
--- EFCoreFluentValidation/Program.cs
+++ EFCoreFluentValidation/Program.cs
@@ -1,4 +1,6 @@
+using EFCoreFluentValidation.Data;
 using EFCoreFluentValidation.ServiceExtensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Registering ApplicationDbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")?? String.Empty));
 
 //Register validators
 builder.Services.AddValidators();
