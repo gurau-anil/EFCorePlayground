26ec91ed0a90eca072391484b8ef7c9f6873ac56
gurau-anil <gurau.anil@gmail.com>
2025-03-18 16:17:40 -0700

    Approach one to connect to the database (Using OnConfiguring method) 1. It is not a recommend approach (we will be using Depenedency injection approoach)

---
+++ EFCoreFluentValidation/Data/ApplicationDbcontext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreFluentValidation.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
